530d65c8dbd5fd54cc68e26ecd00e604589d4cf9
Alex <alex@ascholtes.me>
2017-11-11 13:11:50 -0500

    string to int; also concat

--- Program.cs
+++ Program.cs
@@ -6,8 +6,13 @@ namespace work_tutorials
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Type a message");
-            Console.WriteLine("You said " + Console.ReadLine());
+            Console.WriteLine("What is the name of your band?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How many people are in your band?");
+            int numberOfMembers = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(name + " has " + numberOfMembers + " talented musicians.");
         }
     }
 }
