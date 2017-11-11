7053cc8d4d14fd219d440528edaae44e17979cb8
Alex <alex@ascholtes.me>
2017-11-11 12:05:26 -0500

    c# tutorial

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace work_tutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a message");
+            Console.WriteLine("You said " + Console.ReadLine());
+        }
+    }
+}
