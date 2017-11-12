d45a992c678a7831049fb0d1ed6f7b5c7ef6220a
Alex <alex@ascholtes.me>
2017-11-12 11:03:26 -0500

    Conditions

--- Program.cs
+++ Program.cs
@@ -10,7 +10,13 @@ namespace work_tutorials
             string name = Console.ReadLine();
 
             Console.WriteLine("How many people are in your band?");
-            int numberOfMembers = int.Parse(Console.ReadLine());
+            int numberOfMembers = 0;
+            if (!int.TryParse(Console.ReadLine(), out numberOfMembers));
+            {
+                Console.WriteLine("input was not valid");
+                Enviornment.Exit(0);
+            }
+
 
             Console.WriteLine(name + " has " + numberOfMembers + " talented musicians.");
         }
