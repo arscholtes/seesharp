d3139163720e744307912906d46964e8fd231c73
Alex <alex@ascholtes.me>
2017-11-12 11:13:14 -0500

    Conditions

--- Program.cs
+++ Program.cs
@@ -11,11 +11,16 @@ namespace work_tutorials
 
             Console.WriteLine("How many people are in your band?");
             int numberOfMembers = 0;
-            if (!int.TryParse(Console.ReadLine(), out numberOfMembers));
+            if(numberOfMembers < 0)
             {
-                Console.WriteLine("input was not valid");
-                Enviornment.Exit(0);
+                Console.WriteLine("You must have atleast 1 member in your band.");
             }
+            else if(numberOfMembers == 1)
+                Console.WriteLine(name + " is a solo.");
+            else if(numberOfMembers == 2)
+                Console.WriteLine(name + " is a duo.");
+            else
+                Console.WriteLine(name + " has " + numberOfMembers + "members.");
 
 
             Console.WriteLine(name + " has " + numberOfMembers + " talented musicians.");
